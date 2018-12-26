af3d89e5c17f5a98169956643db07bc5f564e417
Ivan Timofeev <ivan.timofeev@bsc-ideas.com>
2018-12-27 02:26:31 +0300

    #FEUD-7 add rest method for changing json

--- main.go
+++ main.go
@@ -32,7 +32,30 @@ func main() {
 				log.Println(string(body))
 				fmt.Fprint(w, string(body))
 			}
+		}
+	})
 
+	router.HandleFunc("/answers/save", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "OPTIONS":
+			{
+				var httpHeaders = w.Header()
+				httpHeaders.Add("Access-Control-Allow-Origin", "*")
+				fmt.Fprint(w, "")
+			}
+		case "POST":
+			{
+				data, err := ioutil.ReadAll(r.Body)
+				if err != nil {
+					panic(err)
+				}
+				err1 := ioutil.WriteFile("answers.json", data, 0644)
+				if err1 != nil {
+					panic(err1)
+				}
+				var httpHeaders = w.Header()
+				httpHeaders.Add("Access-Control-Allow-Origin", "*")
+			}
 		}
 	})
 
